ec5aefc5796d531c9775e315627d3bbfd6b84f7e
victorftrdba <victornogu80@gmail.com>
2021-07-13 20:51:46 -0300

    formulários com condicionais

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Formulário de inscrição</title>
+</head>
+<body>
+    <p>FORMULÁRIO DE INSCRIÇÃO DE PROFISSIONAIS DE TI</p>
+
+<form action="./script.php" method="POST">
+    <p>Seu nome: <input type="text" name="nome" /></p>
+    <p>Sua idade: <input  type="text" name="idade" /></p>
+    <p><input type="submit" value="Enviar dados do profissional" /></p>
+</form>
+</body>
+</html>
